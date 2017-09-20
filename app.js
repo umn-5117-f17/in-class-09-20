072f737cff3a748021c08ad6e4d1f81fa2dcaeec
Max Harper <maxharp3r@gmail.com>
2017-09-20 10:19:24 -0500

    add stub of handlebars demo

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// set up partials
+// docs: https://github.com/pillarjs/hbs#helpers-and-partials
+// var hbs = require('hbs');
+// hbs.registerPartials(__dirname + '/views/partials');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var data = {
+    title: '5117 in class: 9/20/2017',
+  };
+
+  res.render('index', data);
 });
 
 module.exports = router;
